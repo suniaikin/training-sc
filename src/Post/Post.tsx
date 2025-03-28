4998a8030748cafd2aa3dd7cbe583f9dd34b7e49
suniaikin <class.suniaikin@gmail.com>
2025-03-28 11:44:54 -0700

    Add post user component

---
+++ src/Post/Post.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Post = () => {
+	return (
+		<div>
+			<h2>Test</h2>
+		</div>
+	)
+}
---
+++ src/User/User.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const User = () => {
+	return (
+		<div>
+			<h2>Test</h2>
+		</div>
+	)
+}
